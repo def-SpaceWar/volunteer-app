343d41beeba00063fa6e632de5e91ce01030ab09
Taptaplit Patel <pateltaptaplit@gmail.com>
2021-07-23 23:15:38 -0400

    homepage

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,26 @@ export default function Home() {
       <Head>
         <title>VolunteerUP</title>
         <meta name="description" content="Post & Join Volunteer groups! Helping is happiness!" />
-        <link rel="icon" href="https://cdn.discordapp.com/attachments/865389806376321075/868312279517638736/hackathon_logo_2.png" />
+        <link rel="icon" href="https://cdn.discordapp.com/attachments/865389806376321075/868312279517638736/hackathon_logo_2.png"/>
       </Head>
       <div className={styles.navContainer}>
-        <img className={styles.logo} src="https://cdn.discordapp.com/attachments/865389806376321075/868312279517638736/hackathon_logo_2.png" alt="Voluntierup"/>
+        <img className={styles.logo} src="https://cdn.discordapp.com/attachments/865389806376321075/868312279517638736/hackathon_logo_2.png" alt="VolunteerUP"/>
       </div>
       <div className={styles.mainContainer}>
-        <h1>VolunteerUP</h1>
-        <p>Helping non-profits find volunteer groups</p>
+        <div className={styles.InfoContainer}>
+          <h1 className={styles.cardTitle}>VolunteerUP</h1>
+          <p>C'mon lets be real; without a community, life would be boring 🥱. Thats why we invented VolunteerUP, a way to give back to the community and make it a better place for everyone! 😃</p>
+          <div className={styles.flexContainer}>
+            <button className={styles.joinButton}><strong>Join Now!</strong></button>
+            <div className={styles.hr}></div>
+            <button className={styles.signButton}><strong>Sign In!</strong></button>
+          </div>
+          <p className={styles.cardFooter}>Made with love by VolunteerUP Dev Team ❤️</p>
+        </div>
+        <div className={styles.svgContainer}>
+          <img className={styles.svg} src="https://cdn.discordapp.com/attachments/865389806376321075/868326536883875890/undraw_elements_cipa.png" />
+        </div>
       </div>
     </div>
   )
-}
+} 
