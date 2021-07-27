c8cc3addc1e0c3ece2ca0395e139927d79130eb5
def-SpaceWar <aryanahire9086360780@gmail.com>
2021-07-27 00:33:32 -0400

    fixed a build error

--- pages/form-done.js
+++ pages/form-done.js
@@ -1,34 +1,35 @@
 import Head from 'next/head'
 import styles from '../styles/YAY.module.css'
 import NavBar from './navbar'
+import Link from 'next/link'
 
 //dw abt this taptap or spacewar >:D < glow
 
 export default function Login() {
-  return (
-    <div className={styles.container}>
-        <a href='/'><button className={styles.btn}>Home</button></a>
-        <h1 className={styles.center}>Form Submited!</h1>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-        <div className={styles.confetti}></div>
-    </div>
-  )
-} 
+    return (
+        <div className={styles.container}>
+            <Link href='/'><button className={styles.btn}>Home</button></Link>
+            <h1 className={styles.center}>Form Submited!</h1>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+            <div className={styles.confetti}></div>
+        </div>
+    )
+}
