e8b5c29285d5d2bc303d82be09022bbab8c563c8
def-SpaceWar <aryanahire9086360780@gmail.com>
2021-07-27 18:59:31 -0400

    Got Stuff Done (Made good project cards)

--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -1,12 +1,16 @@
-import {useRouter} from 'next/router'
-import React, {useState, useEffect} from 'react'
 import Image from 'next/image'
 import NavBar from '../navbar'
 import styles from '../../styles/Infopage.module.css'
-import ProjectCard from '../projects'
 import database from '../firebaseInit'
+import { useState } from 'react'
 
 const db = database.firestore()
+
+export function getServerSideProps(context) {
+    return {
+        props: { params: context.params }
+    };
+}
 
 // const db = [
 //     {
@@ -14,38 +18,40 @@ const db = database.firestore()
 //         imgSrc: 'https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj',
 //         name: 'St. Judes',
 //         description: "St. Judes is an amazing organization working to research cancer and save childrens' lives!"
+//         location: 'Memphis Tennessee'
 //     }
 // ]
 
-const Post = () => {
-    const router = useRouter()
-    const [data, setData] = useState(false);
-    const { pid } = router.query
-    let imgSrc = '/hackathon_logo.png'
-    let name = 'Insert Name Here'
-    let description = 'Insert Description Here'
+const Post = ({ params }) => {
+    let data;
+    let [global, setGlobal] = useState({
+        imgSrc: '/hackathon_logo.png',
+        name: '404',
+        description: 'This project isnt found',
+        location: 'Your Mom\'s House'
+    });
+
+    const pid = params.pid;
 
     db.collection("Projects").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             if (doc.id == pid) {
-                setData(doc.data());
+                data = doc.data();
+                setGlobal({
+                    imgSrc: data.image,
+                    name: data.name,
+                    description: data.description,
+                    location: data.location,
+                    email: data.email,
+                    members: data.members,
+                    phone: data['phone number']
+                });
             }
         });
-    });    
-    
-    // useEffect(() => {
-    if (data) {
-        console.log(data.imgSrc);
-        for (let i = 0; i < data.length; i++) {
-            imgSrc = data[i].image;
-            name = data[i].name;
-            description = data[i].description;
-        }
-    }
-    // }, [data])
+    });
 
-    return (    
-        <div className={styles.container}>
+    return (
+        <div className={styles.container} >
             <NavBar />
             <br />
             <br />
@@ -55,18 +61,22 @@ const Post = () => {
             <div className={styles.postContainer}>
                 <div className={styles.flexRow}>
                     <div className={styles.postImage}>
-                        <Image className={styles.postImage} src={imgSrc} width="500%" height="500%" />
+                        <Image className={styles.postImage} src={global.imgSrc} width="500%" height="500%" />
                         <div className>
-                            <h1 className={styles.postName}>{name}</h1>
+                            <h1 className={styles.postName}>{global.name}</h1>
                             <button className={styles.postButton}>Join Now</button>
                             <br />
-                            <button className={styles.postButton} onClick={() => setModalOpen(!modalOpen)}>Share</button>
+                            <button className={styles.postButton} >Share</button>
                         </div>
                     </div>
                 </div>
-                <p className={styles.postDescription}>{description}</p>
+                <p className={styles.postLocation}>Location: {global.location}</p>
+                <p className={styles.postMembers}>Members: {global.members}</p>
+                <p className={styles.postEmail}>Email: {global.email}</p>
+                <p className={styles.postPhone}>Phone Number: {global.phone}</p>
+                <p className={styles.postDescription}>{global.description}</p>
             </div>
-        </div>
+        </div >
     )
 }
 
--- pages/projects.js
+++ pages/projects.js
@@ -3,10 +3,11 @@ import Image from 'next/image'
 import styles from '../styles/Projects.module.css'
 import NavBar from './navbar'
 import firebase from './firebaseInit';
+import { useState, useEffect } from 'react';
 
 const db = firebase.firestore()
 
-function ProjectCard({name, url, description, img}) {
+function ProjectCard({ name, url, description, img, time }) {
     if (description.length > 140) {
         let desc = ''
         for (let i = 0; i < 77; i++) {
@@ -14,6 +15,19 @@ function ProjectCard({name, url, description, img}) {
         }
         description = desc + '...'
     }
+
+
+    let diff_time = Date.now() - (time.seconds * 1000);
+    let days = (diff_time - (diff_time % 86400000)) / 86400000;
+    diff_time = diff_time - (days * 86400000);
+    let hours = (diff_time - (diff_time % 3600000)) / 3600000;
+    diff_time = diff_time - (hours * 3600000);
+    let minutes = (diff_time - (diff_time % 60000)) / 60000;
+    diff_time = diff_time - (minutes * 60000);
+    let seconds = (diff_time - (diff_time % 1000)) / 1000;
+
+
+
     return (
         <div>
             <div className={styles.projCard}>
@@ -21,21 +35,26 @@ function ProjectCard({name, url, description, img}) {
                 <div>
                     <h3 className={styles.projCardTitle}>{name}</h3>
                     <p className={styles.projCardDesc}>{description}</p>
-                    <a href={url}><button className={styles.projCardLink}>Learn More</button></a>
+                    <p className={styles.projCardTimeStamp}>{days} Days, {hours} Hours, {minutes} Minutes, {seconds} Seconds</p>
+                    <a href={`/post/${url}`}><button className={styles.projCardLink}>Learn More</button></a>
                 </div>
             </div>
         </div>
     )
 }
 
-// db.collection("Projects").get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         const data = doc.data()
-//         console.log(data)
-//     });
-// });
-
 export default function Projects() {
+    const [recent, setRecent] = useState([]);
+
+    useEffect(() => {
+        db.collection("Projects").orderBy('createdAt').get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                let data = doc.data();
+                setRecent([...recent, (<ProjectCard name={data.name} url={doc.id} description={data.description} img={data.image} time={data.createdAt} />)])
+            });
+        })
+    }, [])
+
     return (
         <div className={styles.container}>
             <Head>
@@ -43,35 +62,20 @@ export default function Projects() {
                 <meta name="description" content="Post & Join Volunteer groups! Helping is happiness!" />
             </Head>
             <NavBar />
-      <br />
-      <br />
-      <br />
-            <h1 className={styles.projTitle}>Recent</h1>
+            <br />
+            <br />
+            <br />
+            <h1 className={styles.projTitle}>Most Recent</h1>
             <div className={styles.slider}>
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
+                {
+                    recent.map((item) => {
+                        return item;
+                    })
+                }
             </div>
             <h1 className={styles.projTitle}>Most Members</h1>
             <div className={styles.slider}>
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-            </div>
-            <h1 className={styles.projTitle}>Other</h1>
-            <div className={styles.slider}>
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
-                <ProjectCard name="St Jude's" url='/post/gxi1Qu3eFHnGZkmebgPS' description="St. Jude is leading the way the world understands, treats and defeats childhood cancer and other life-threatening diseases." img='https://yt3.ggpht.com/ytc/AKedOLTJtWKMxZXxgvr20Oy0jglCSpxta-vVgnYJmI-kDg=s900-c-k-c0x00ffffff-no-rj' />
+
             </div>
         </div>
     )
