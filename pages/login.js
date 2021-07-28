4abd0e98f5b2d1552ed43f7a24fc229184abba6f
def-SpaceWar <aryanahire9086360780@gmail.com>
2021-07-28 17:06:57 -0400

    Added [Enter] to make the search bar automatically select the first option

--- pages/login.js
+++ pages/login.js
@@ -5,49 +5,49 @@ import firebase from './firebaseInit';
 import {useState} from 'react';
 
 export default function Login() {
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
-
-  function authUser(e, email, password) {
-    e.preventDefault()
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      // ...
-      console.log(user)
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
     })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      return console.log(errorMessage)
-    });
-  }
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>VolunteerUP | Login</title>
-      </Head>
-      <NavBar />
-      <br />
-      <br />
-      <br />
-      <div className={styles.centerForm}>
-          <form>
-              <h1>Login</h1>
-              <input placeholder="Email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-              <br />
-              <input type="password" placeholder="Password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
-              <br />
-              <center>
-                <button type="submit" onClick={(e) => authUser(e, formData.email, formData.password)}>Submit</button>
-              </center>
-          </form>
-      </div>
-    </div>
-  )
-} 
+    function authUser(e, email, password) {
+        e.preventDefault()
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+                console.log(user)
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage, errorCode);
+                return;
+            });
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>VolunteerUP | Login</title>
+            </Head>
+            <NavBar />
+            <br />
+            <br />
+            <br />
+            <div className={styles.centerForm}>
+                <form>
+                    <h1>Login</h1>
+                    <input placeholder="Email" onChange={(e) => setFormData({...formData, email: e.target.value})} />
+                    <br />
+                    <input type="password" placeholder="Password" onChange={(e) => setFormData({...formData, password: e.target.value})} />
+                    <br />
+                    <center>
+                        <button type="submit" onClick={(e) => authUser(e, formData.email, formData.password)}>Submit</button>
+                    </center>
+                </form>
+            </div>
+        </div>
+    )
+}
--- pages/navbar.js
+++ pages/navbar.js
@@ -1,101 +1,109 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect, useCallback, useRef } from 'react'
+import {useState, useEffect, useCallback, useRef} from 'react'
 import firebase from './firebaseInit';
 
 const db = firebase.firestore()
 
 export default function NavBar() {
-  let data;
-  const [results, setResults] = useState([]);
-  const [dropDown, setDropDown] = useState([]);
-  const searchRef = useRef(null)
-  const [active, setActive] = useState(false)
+    let data;
+    const [results, setResults] = useState([]);
+    const [dropDown, setDropDown] = useState([]);
+    const searchRef = useRef(null)
+    const [active, setActive] = useState(false)
 
-  useEffect(() => {
-    db.collection("Projects").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-            data = doc.data();
-            setResults([
-                ...results,
-                {
-                  name: data.name,
-                  location: data.location,
-                  id: doc.id
-                }
-            ]);
-      });
-    });
-  }, [])
+    useEffect(() => {
+        db.collection("Projects").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                data = doc.data();
+                setResults([
+                    ...results,
+                    {
+                        name: data.name,
+                        location: data.location,
+                        id: doc.id
+                    }
+                ]);
+            });
+        });
+    }, [])
 
-  function getSearchDropdown(input) {
-    const guess = results.filter(post => post.name.toLowerCase().includes(input) || post.location.toLowerCase().includes(input))
-    if (guess.length > 5) {
-      guess.length = 5;
+    function getSearchDropdown(input) {
+        const guess = results.filter(post => post.name.toLowerCase().includes(input) || post.location.toLowerCase().includes(input))
+        if (guess.length > 5) {
+            guess.length = 5;
+        }
+        setDropDown(guess);
     }
-    setDropDown(guess);
-  }
 
-  const onFocus = useCallback(() => {
-    setActive(true)
-    window.addEventListener('click', onClick)
-  }, [])
+    const onFocus = useCallback(() => {
+        setActive(true)
+        window.addEventListener('click', onClick)
+    }, [])
 
-  const onClick = useCallback((event) => {
-    if (searchRef.current && !searchRef.current.contains(event.target)) {
-      setActive(false)
-      window.removeEventListener('click', onClick)
-    }
-  }, [])
+    const onClick = useCallback((event) => {
+        if (searchRef.current && !searchRef.current.contains(event.target)) {
+            setActive(false)
+            window.removeEventListener('click', onClick)
+        }
+    }, [])
 
-  return (
-    <div className={styles.navContainer}>
-      <div className={styles.centerSearch}>
-        <form ref={searchRef}>
-          <input list="autoGuess" type="text" placeholder="Search..." onChange={(e) => getSearchDropdown(e.target.value)} onFocus={onFocus}/>
-          { active && dropDown.length > 0 && (
-            <ul className={styles.results}>
-              {dropDown.map(({ name, id }) => (
-                <li className={styles.result} key={id}>
-                  <Link href="/post/[pid]" as={`/post/${id}`}>
-                    <a>{name}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </form>
-      </div>
-      <div className={styles.centerLogo}>
-        <Link href="/" className={styles.logo}>
-          <Image
-            src="/hackathon_logo.png"
-            alt="VolunteerUP"
-            width="100%"
-            height="100%"
-          />
-        </Link>
-      </div>
-      <div className={styles.centerLogo}>
-        <ui className={styles.navLinks}>
-          <li>
-            <Link href="/login">
-              <p className={styles.navItem}>Login</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/signup">
-              <p className={styles.navItem}>Sign Up</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/projects">
-              <p className={styles.navItem}>Projects</p>
-            </Link>
-          </li>
-        </ui>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.navContainer}>
+            <div className={styles.centerSearch}>
+                <form ref={searchRef}>
+                    <input list="autoGuess" type="text" placeholder="Search..." onChange={(e) => getSearchDropdown(e.target.value)} onFocus={onFocus} onKeyPress={
+                        (e) => {
+                            if (e.key == "Enter") {
+                                e.preventDefault();
+                                const guess = results.filter(post => post.name.toLowerCase().includes(e.target.value) || post.location.toLowerCase().includes(e.target.value))
+                                window.location.replace(`/post/${guess[0].id}`)
+                            }
+                        }
+                    } />
+                    {active && dropDown.length > 0 && (
+                        <ul className={styles.results}>
+                            {dropDown.map(({name, id}) => (
+                                <li className={styles.result} key={id}>
+                                    <Link href="/post/[pid]" as={`/post/${id}`}>
+                                        <a>{name}</a>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </form>
+            </div>
+            <div className={styles.centerLogo}>
+                <Link href="/" className={styles.logo}>
+                    <Image
+                        src="/hackathon_logo.png"
+                        alt="VolunteerUP"
+                        width="100%"
+                        height="100%"
+                    />
+                </Link>
+            </div>
+            <div className={styles.centerLogo}>
+                <ui className={styles.navLinks}>
+                    <li>
+                        <Link href="/login">
+                            <p className={styles.navItem}>Login</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/signup">
+                            <p className={styles.navItem}>Sign Up</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/projects">
+                            <p className={styles.navItem}>Projects</p>
+                        </Link>
+                    </li>
+                </ui>
+            </div>
+        </div>
+    );
 }
