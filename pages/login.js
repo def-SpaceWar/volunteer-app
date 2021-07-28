395b9429b382717141f7852f152fa30c60a52d41
Taptaplit Patel <pateltaptaplit@gmail.com>
2021-07-28 16:43:59 -0400

    login/signup made

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,32 @@
 import Head from 'next/head'
 import styles from '../styles/Form.module.css'
 import NavBar from './navbar'
+import firebase from './firebaseInit';
+import {useState} from 'react';
 
 export default function Login() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  function authUser(e, email, password) {
+    e.preventDefault()
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+      console.log(user)
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return console.log(errorMessage)
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,12 +39,12 @@ export default function Login() {
       <div className={styles.centerForm}>
           <form>
               <h1>Login</h1>
-              <input placeholder="Email" />
+              <input placeholder="Email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
               <br />
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
               <br />
               <center>
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={(e) => authUser(e, formData.email, formData.password)}>Submit</button>
               </center>
           </form>
       </div>
--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,35 @@
 import Head from 'next/head'
 import styles from '../styles/Form.module.css'
 import NavBar from './navbar'
+import firebase from './firebaseInit';
+import {useState} from 'react';
 
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirm_password: '',
+  })
+
+  function createUser(email, password, confirm_password, e) {
+    e.preventDefault()
+    if (password !== confirm_password || email === '' || password === '') {
+      return console.log('Passwords Do not Match')
+    }
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return console.log(error.messsage)
+    });
+
+
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -16,14 +43,14 @@ export default function SignUp() {
       <div className={styles.centerForm}>
           <form>
               <h1>Sign Up</h1>
-              <input placeholder="Email" />
+              <input placeholder="Email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
               <br />
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
               <br />
-              <input type="password" placeholder="Confirm Password" />
+              <input type="password" placeholder="Confirm Password" onChange={(e) => setFormData({ ...formData, confirm_password: e.target.value })} />
               <br />
               <center>
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={(e) => createUser(formData.email, formData.password, formData.confirm_password, e)}>Submit</button>
               </center>
           </form>
       </div>
