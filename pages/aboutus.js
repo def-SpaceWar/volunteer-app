3920a525376f7c50504775602617a89474329e75
Taptaplit Patel <pateltaptaplit@gmail.com>
2021-07-30 18:25:08 -0400

    tiny fix in about us

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from &apos;next/head'
 import styles from '../styles/Form.module.css'
 import NavBar from './navbar'
 import Link from 'next/link'
@@ -12,7 +12,7 @@ export default function AboutUS() {
         <NavBar />
         <div className={styles.aboutCard}>
             <h1 className={styles.aboutTitle}>About Us</h1>
-            <p className={styles.aboutText}>We started this non-profit after we noticed, there weren't many places to find some non-profits to help out. So we had only one idea... Create our own website to help others find non-profits. This site is built so if you are a student or volunteer and want to help out you can by searching for non profits. If you are a host and own a non profit where you need volunteers you can create a post and have people discover how you help the community out, and if they are intrested they can join too!</p>
+            <p className={styles.aboutText}>We started this non-profit after we noticed, there weren&apos;t many places to find some non-profits to help out. So we had only one idea... Create our own website to help others find non-profits. This site is built so if you are a student or volunteer and want to help out you can by searching for non profits. If you are a host and own a non profit where you need volunteers you can create a post and have people discover how you help the community out, and if they are intrested they can join too!</p>
         </div>
     </div>
   )
